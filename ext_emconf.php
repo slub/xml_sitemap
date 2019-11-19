e83a88c1d6338424e4f1d06482919147d9ce1dcb
Alexander Bigga <alexander.bigga@slub-dresden.de>
2019-11-19 13:55:37 +0100

    extension is working with TYPO3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
